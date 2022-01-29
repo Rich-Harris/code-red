42d0192747bc35a373176cfc753daa21edc3e7da
Simon <simon.holthausen@accso.de>
2022-01-29 23:44:32 +0100

    indentation, docs

--- src/utils/push_array.js
+++ src/utils/push_array.js
@@ -1,14 +1,12 @@
 /**
- * Does `array.push` for all `items`.
- * Needed because `array.push(...items)` throws
- * "Maximum call stack size exceeded" when items
- * is too big of an array
+ * Does `array.push` for all `items`. Needed because `array.push(...items)` throws
+ * "Maximum call stack size exceeded" when `items` is too big of an array.
+ *
  * @param {any[]} array 
  * @param {any[]} items 
  */
- export function push_array(array, items) {
-    for (let i = 0; i < items.length; i++) {
-      array.push(items[i]);
-    }
-  };
-  
+export function push_array(array, items) {
+	for (let i = 0; i < items.length; i++) {
+		array.push(items[i]);
+	}
+};
