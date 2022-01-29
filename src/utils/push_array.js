c22495d560d08fcdb56a6a5b9e54b54e7cea8364
milahu <milahu@gmail.com>
2022-01-29 10:31:30 +0100

    fix var names

--- src/utils/push_array.js
+++ src/utils/push_array.js
@@ -1,11 +1,11 @@
-export function push_array(thisArray, ...otherList) {
+export function push_array(this_array, ...other_array_list) {
   let count = 0;
-  for (let a = 0; a < otherList.length; a++) {
-    const other = otherList[a];
-    for (let i = 0; i < other.length; i++) {
-      thisArray.push(other[i]);
+  for (let a = 0; a < other_array_list.length; a++) {
+    const other_array = other_array_list[a];
+    for (let i = 0; i < other_array.length; i++) {
+      this_array.push(other_array[i]);
     }
-    count += other.length;
+    count += other_array.length;
   }
   return count;
 };
